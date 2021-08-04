49120ebfaadb2d8a93641fd564f93012ae41589b
Spencer Dunlap <spencerdunlap@me.com>
2021-08-03 21:56:19 -0500

    minor update

--- components/views/Bio.js
+++ components/views/Bio.js
@@ -1,7 +1,7 @@
 import html from "html-literal";
 import dogPic from "../../assets/img/photo-1510771463146.jpg";
 
-export default (st) => html`
+export default st => html`
   <section id="bio">
     <h2>Vivamus ac justo eu nisi</h2>
     <img src="${dogPic}" alt="me" />
