89368bca5d37953237d1af3ea4773ad49cff0c3a
Spencer Dunlap <spencerdunlap@me.com>
2021-08-11 21:28:31 -0500

    MongoDB

--- server/index.js
+++ server/index.js
@@ -1,10 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const greetings = require("./routers/greetings");
 
 // Import statements can go above^^^^
+// Instantiation
 const app = express();
 
-// Configuring Express Instance
+mongoose.connect("mongodb://localhost/pizzeria");
+const db = mongoose.connection;
 
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+
+// Define middleware functions
+const logging = (request, response, next) => {
+  console.log(`${Date.now()} ${request.method} ${request.url}`);
+  next();
+};
+
+// Using middleware
+app.use(express.json());
+app.use(logging);
+
+app.use(greetings);
+
+// Configuring Express Instance
 app.get("/status", (request, response) => {
   response.send(JSON.stringify({ message: "Service healthy" }));
 });
@@ -24,10 +48,6 @@ app
     );
   });
 
-app.route("/greet/:name").get((request, response) => {
-  const name = request.params.name;
-  response.status(418).json({ message: `Hello ${name}` });
-});
-
 // Executing the Express app (This must ALWAYS be last)
-app.listen(4040, () => console.log("Listening on port 4040"));
+const port = process.env.PORT || 4040;
+app.listen(port, () => console.log(`Listening on port ${port}`));
